014d87380a903daa3eb36d0a5cd26c2470c45bf5
yiv <yuefengshao@126.com>
2021-01-15 20:27:18 +0800

    add comments

--- README.md
+++ README.md
@@ -1,11 +1,52 @@
-# full_search
+# Full_search
 
 This is a full text search Flutter plugin which build on Tantivy.
 
 Tantivy is a full-text search engine library inspired by Apache Lucene and written in Rust.
 
+## Schema
 
-## example 
+A FieldType describes the type (text, u64) of a field as well as how it should be handled.
+
+Schema field type supported:
+
+| Field Type | Description |
+|:---|:---|
+| string |  String field type configuration|
+| text |  Unsigned 64-bits integers field type configuration |
+| u64 |  Signed 64-bits integers 64 field type configuratio|
+| i64 |  64-bits float 64 field type configuration|
+| date | Signed 64-bits Date 64 field type configuration|
+| facet |  Hierachical Facet|
+| bytes |Bytes (one per document)|
+
+## Example
+
+
+``` dart
+
+/// create a instance of SearchEngine and set it up
+final engine = SearchEngine();
+SearchEngine.setup();
+
+/// get the path which used to store index files
+final _path = await getApplicationDocumentsDirectory().path;
+
+/// define the schema of the data which wanted to be indexed
+final _schema = r'{"id": "i64",  "timestamp": "date", "content": "text"}';
+
+/// open a exists one or create a new one on the device
+engine.openOrCreate(_path, _schema);
+
+/// encode the data object to a json string
+final _doc = jsonEncode(dataObject);
+/// start to index and store the data
+engine.index(_doc);
+
+/// give the query keywords and the field which to search on 
+final res = await engine.search('关键字 关键词', ["content"], 1, 10);
+
+```
 
 ![](https://github.com/yiv/full_search/blob/master/example/example.png)
 
--- lib/full_search.dart
+++ lib/full_search.dart
@@ -6,15 +6,28 @@ import 'dart:async';
 import 'dart:convert';
 
 class SearchEngine {
+
+  /// setup the engine prerequisities
   static setup() {
     native.store_dart_post_cobject(NativeApi.postCObject);
     print("Scrap Setup Done");
   }
+
+
+  /// open a exist index or create a new directory to store index files
+  /// path: the location path of directory on device
+  /// schema: a json format string which defines the field data types of object
+  /// for example:
+  /// ```
+  /// {"message_id": "i64","user_id": "i64", "guild_id": "i64", "channel_id": "i64", "timestamp": "date", "content": "text"}
+  /// ```
   int openOrCreate(String path, String schema) {
     final _path = Utf8.toUtf8(path);
     final _schema = Utf8.toUtf8(schema);
     return native.se_open_or_create(_path, _schema);
   }
+
+  /// test whether the engin has been created
   Future<bool> exists() {
     final completer = Completer<bool>();
     final sendPort = singleCompletePort(completer);
@@ -25,6 +38,9 @@ class SearchEngine {
     }
     return completer.future;
   }
+
+  /// store and index the object
+  /// doc: a json format string of the object which want to be indexed
   Future<void> index(String doc) {
     final _doc = Utf8.toUtf8(doc);
     final completer = Completer<String>();
@@ -38,6 +54,9 @@ class SearchEngine {
   }
 
 
+  /// query and search the specified fields
+  /// query: keywords used for search
+  /// fields: search for the content of which fields
   Future<String> search(String query, List fields, int pageStart, int pageSize) {
     final _query = Utf8.toUtf8(query);
     final _fields = Utf8.toUtf8(json.encode(fields));
@@ -52,6 +71,8 @@ class SearchEngine {
     }
     return completer.future;
   }
+
+  /// retrive a specify document by giving a field of i64 and it's value
   Future<String> getByI64( String field, int id) {
     final _field = Utf8.toUtf8(field);
     final completer = Completer<String>();
@@ -65,6 +86,8 @@ class SearchEngine {
     }
     return completer.future;
   }
+
+  /// retrive a specify document by giving a field of u64 and it's value
   Future<String> getByU64( String field, int id) {
     final _field = Utf8.toUtf8(field);
     final completer = Completer<String>();
